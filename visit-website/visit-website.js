e912a855a17e6219a1b6865e25fc08a7e0fa9172
ailoha <134615514+ailoha@users.noreply.github.com>
2023-09-06 23:04:31 +0800

    Create visit-website.js

---
+++ visit-website/visit-website.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const websites = require('./websites.json');  // 使用 require 读取 JSON 文件
+
+// 创建一个函数来处理网站访问和等待
+async function visitWebsite(page, url) {
+  await page.goto(url);
+  await page.waitForTimeout(60000); // 等待 60 秒
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // 使用 forEach 循环来访问每个网站
+  for (const website of websites) {
+    await visitWebsite(page, website);
+  }
+
+  await browser.close();
+})();
